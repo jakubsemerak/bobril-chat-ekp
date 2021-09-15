24c68da1aa10aa832ef4f2c41d75ecc798b6ea62
Jakub Semerák <j.semerak@quadient.com>
2021-09-15 10:01:43 +0200

    Styles revert back.

--- src/components/chatSidebarHeader.tsx
+++ src/components/chatSidebarHeader.tsx
@@ -2,6 +2,7 @@ import * as b from "bobril";
 import {create as Filter} from "bobwai--filter";
 import {create as HeaderText, TextStyle} from "bobwai--header-text";
 import Avatar from "bobwai--avatar";
+import {chatSidebarHeaderUserInfoStyle, chatSidebarHeaderWrapperStyle} from "../styles";
 
 export interface IChatSidebarHeaderData {
     avatar?: string;
@@ -14,15 +15,18 @@ export class ChatSidebarHeader extends b.Component<IChatSidebarHeaderData> {
     render(): b.IBobrilChildren {
         return (
             <>
-                <HeaderText content={this.data.name} leftIcon={
-                    <Avatar imageSrc={this.data.avatar} size={101}/>
-                } textStyle={TextStyle.Subtitle200}/>
-
-                <Filter placeholder={"Search"} onChange={(v) => {
-                    this.data.onFilterChange(v);
-                }} onTextClear={() => {
-                    this.data.onFilterChange(undefined);
-                }}/>
+                <div style={chatSidebarHeaderWrapperStyle}>
+                    <div style={chatSidebarHeaderUserInfoStyle}>
+                        <HeaderText content={this.data.name} leftIcon={
+                            <Avatar imageSrc={this.data.avatar} size={101}/>
+                        } textStyle={TextStyle.Subtitle200}/>
+                    </div>
+                    <Filter placeholder={"Search"} onChange={(v) => {
+                        this.data.onFilterChange(v);
+                    }} onTextClear={() => {
+                        this.data.onFilterChange(undefined);
+                    }}/>
+                </div>
             </>
         );
     }
---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import * as b from "bobril";
+import * as Font from "bobwai--font";
+
+export const chatSidebarHeaderUserInfoStyle = b.styleDef({
+    padding: "8px",
+    lineHeight: "101px",
+});
+
+export const chatSidebarHeaderWrapperStyle = b.styleDef({
+    padding: "8px",
+});
