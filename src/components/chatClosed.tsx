2af723b501fab5fc71693699521e9ce1b543ebea
Jakub Semerák <j.semerak@quadient.com>
2021-09-15 10:24:43 +0200

    Center closed chat.

--- src/components/chatClosed.tsx
+++ src/components/chatClosed.tsx
@@ -1,13 +1,13 @@
 import * as b from "bobril";
 import {IRouteHandlerData} from "bobril";
 import {create as EmptyState, Size} from "bobwai--empty-state/src/lib";
+import {ChildrenDisplayType, HorizontalCenterType, LCenter, VerticalCenterType} from "bobwai--l-center"
 
-export class ChatClosed extends b.Component<IRouteHandlerData> {
-    render(): b.IBobrilChildren {
-        return (
-            <>
-                <EmptyState size={Size.Large} message={"No chat selected."}/>
-            </>
-        );
-    }
+export function ChatClosed(data: IRouteHandlerData): b.IBobrilNode {
+    return (
+        <LCenter verticalCenterType={VerticalCenterType.Center} horizontalCenterType={HorizontalCenterType.Center}
+                 childrenDisplayType={ChildrenDisplayType.InlineBlock} height={800}>
+            <EmptyState size={Size.Large} message={"No chat selected."}/>
+        </LCenter>
+    );
 }
